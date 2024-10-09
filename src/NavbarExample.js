badd088a0bd9eb5fef33331faf2e36121db7ff35
gurbur <jihwan7577@gmail.com>
2024-10-09 13:16:36 +0900

    Feat: align navbar's buttons to right

--- src/NavbarExample.js
+++ src/NavbarExample.js
@@ -10,64 +10,76 @@ function NavbarExample({ isLogin, onClickLogin }) {
   return (
     <>
       {[false].map((expand) => (
-        <Navbar key={expand} expand={expand} className="bg-body-tertiary mb-3">
-          <Container fluid>
-            <Navbar.Brand href="#">Blog Example</Navbar.Brand>
-            <Button classname="login-button" onClick={onClickLogin}>{isLogin ? "Logout" : "Login"}</Button>
-            <Profile isLogin={isLogin} _onClick={onClickLogin} />
-            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
-            <Navbar.Offcanvas
-              id={`offcanvasNavbar-expand-${expand}`}
-              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-              placement="end"
+  <Navbar key={expand} expand={expand} className="bg-body-tertiary mb-3">
+    <Container fluid>
+      <Navbar.Brand href="#">Blog Example</Navbar.Brand>
+
+      <div className="ms-auto d-flex align-items-center">
+        <Button className="login-button me-2" onClick={onClickLogin}>
+          {isLogin ? "Logout" : "Login"}
+        </Button>
+        {isLogin && (
+          <Profile isLogin={isLogin} _onClick={onClickLogin} />
+        )}
+      </div>
+
+      <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+      <Navbar.Offcanvas
+        id={`offcanvasNavbar-expand-${expand}`}
+        aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+        placement="end"
+      >
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+            Offcanvas
+          </Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <Nav className="justify-content-end flex-grow-1 pe-3">
+            <Nav.Link href="#action1">Home</Nav.Link>
+            <Nav.Link href="#action2">Link</Nav.Link>
+            <NavDropdown
+              title="Dropdown"
+              id={`offcanvasNavbarDropdown-expand-${expand}`}
             >
-              <Offcanvas.Header closeButton>
-                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                  Offcanvas
-                </Offcanvas.Title>
-              </Offcanvas.Header>
-              <Offcanvas.Body>
-                <Nav className="justify-content-end flex-grow-1 pe-3">
-                  <Nav.Link href="#action1">Home</Nav.Link>
-                  <Nav.Link href="#action2">Link</Nav.Link>
-                  <NavDropdown
-                    title="Dropdown"
-                    id={`offcanvasNavbarDropdown-expand-${expand}`}
-                  >
-                    <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action4">
-                      Another action
-                    </NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action5">
-                      Something else here
-                    </NavDropdown.Item>
-                  </NavDropdown>
-                </Nav>
-                <Form className="d-flex">
-                  <Form.Control
-                    type="search"
-                    placeholder="Search"
-                    className="me-2"
-                    aria-label="Search"
-                  />
-                  <Button variant="outline-success">Search</Button>
-                </Form>
-              </Offcanvas.Body>
-            </Navbar.Offcanvas>
-          </Container>
-        </Navbar>
-      ))}
+              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">
+                Something else here
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Offcanvas.Body>
+      </Navbar.Offcanvas>
+    </Container>
+  </Navbar>
+))}
+
+
     </>
   );
 }
 
 function Profile({ isLogin, _onClick }) {
-  let content;
   if (isLogin) {
-    content = <Button classname="user-profile">Profile</Button>
+    return (
+      <Button className="user-profile">Profile</Button>
+    );
   }
-  return (<div>{content}</div>);
+  return null;
 }
 
 export default NavbarExample;
